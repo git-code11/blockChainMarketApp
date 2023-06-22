e8c47c044341078e5aa5cac0d8306742fb1089a2
git-code11 <83471862+git-code11@users.noreply.github.com>
2023-06-22 12:23:19 +0100

    Update next.config.js

--- shield-dapp/next.config.js
+++ shield-dapp/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  staticPageGenerationTimeout:120,
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (!isServer || true) {
