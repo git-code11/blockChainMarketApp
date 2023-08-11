9ea54c056544248a85bb43f1aa6f99be9544e73f
Victor <vector.code11@gmail.com>
2023-08-11 17:33:57 +0100

    set wallet version to 2

--- shield-dapp/context/provider/wallet.js
+++ shield-dapp/context/provider/wallet.js
@@ -19,7 +19,7 @@ const chains = ctxConfig.activeChains//[bscTestnet, bsc, goerli, mainnet];
 const { provider } = configureChains(chains, [w3mProvider({ projectId })])
 const wagmiClient = createClient({
   autoConnect: true,
-  connectors: w3mConnectors({ version: 1, chains, projectId }),
+  connectors: w3mConnectors({ version: 2, chains, projectId }),
   provider
 });
 
